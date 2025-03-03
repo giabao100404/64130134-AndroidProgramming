69dacda4e2af068e47d4141fec72e96c400a65e7
giabao100404 <tbao100404@gmail.com>
2025-03-03 08:50:20 +0700

    xử lý cài đặt Onclick trên XML

--- appCong2so/app/src/main/java/tiil/edu/appcong2so/MainActivity.java
+++ appCong2so/app/src/main/java/tiil/edu/appcong2so/MainActivity.java
@@ -1,24 +1,39 @@
 package tiil.edu.appcong2so;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    //Đây là bộ lắng nghe và xử lý xự kiện click lên nút Tính Tổng
+    public void XuLyCong(View view){
+        //Tìm tham chiếu đến điều khiển trên tệp XML, mapping sang JAVA file
+        EditText editTextsoA = findViewById(R.id.edtA);
+        EditText editTextsoB = findViewById(R.id.edtB);
+        EditText editTextsoKQ = findViewById(R.id.edtKQ);
+
+        //Lấy dữ liệu về ở điều kiện số a:
+        String strA = editTextsoA.getText().toString();          //strA = "2"
+        //Lấy dữ liệu về ở điều kiện số b:
+        String strB = editTextsoB.getText().toString();          //strB = "4"
+
+        //Chuyển dữ liệu sang dạng số
+                    int so_A = Integer.parseInt(strA);
+                    int so_B = Integer.parseInt(strB);
+
+        //Tính toán theo yêu cầu
+        int tong = so_A + so_B;
+        String strTong = String.valueOf(tong);          //chuyển sang dạng chuỗi
+
+        //Hiện ra màn hình
+        editTextsoKQ.setText(strTong);
     }
 }
