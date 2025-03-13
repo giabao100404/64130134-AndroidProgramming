1b9877868bc01e6eeb9c083653650c8becd7b3f9
giabao100404 <tbao100404@gmail.com>
2025-03-13 08:48:33 +0700

    manhinhdongian

---
+++ EX6_manhinhdongian/app/src/main/java/tiil/edu/ex6_manhinhdongian/MainActivity.java
@@ -0,0 +1,53 @@
+package tiil.edu.ex6_manhinhdongian;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button nutMH2;
+    Button nutMH3;
+    void TimDieuKhien()
+    {
+        nutMH2 = (Button) findViewById(R.id.btnMH2);
+        nutMH3 = (Button) findViewById(R.id.btnMH3);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        //Tìm điều khiển nút bấm
+        TimDieuKhien();
+        //Gắn bộ lắng nghe sự kiện
+        nutMH2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo một Intent 2 tham số: 1) màn hình hiển thị tại this 2) màn hình hiểu thị tại class
+                Intent intentMH2 = new Intent(MainActivity.this, MH2Activity.class);
+                //B2. Gửi
+                startActivities(intentMH2);
+
+            }
+        });
+        nutMH3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý chuyển màn hình
+                //B1. Tạo một Intent 2 tham số: 1) màn hình hiển thị tại this 2) màn hình hiểu thị tại class
+                Intent intentMH3 = new Intent(MainActivity.this, MH3Activity.class);
+                //B2. Gửi
+                startActivities(intentMH3);
+
+            }
+    }
+}
